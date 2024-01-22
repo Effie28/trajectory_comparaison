554455dfa40729c9fdf9449c5833798060436c3e
Effie <efdau@ulaval.ca>
2024-01-22 11:04:23 -0500

    Updated data and modified README

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # trajectory_comparaison
+
+Library to generate trajectories from GPS, RTS and ICP data and compare them to a reference trajectory.
+
+## To plot trajectories compared to a reference :
+
+### Generate reconstructed_traj
+python3 scripts/trajectory_generation.py -gps trajectories/gt_gps_traj1.csv -rts trajectories/gt_rts_traj1_timestamp_modified.csv -icp trajectories/icp_odom_traj1_timestamp_modified.csv -ps
+
+### ICP / IMU / Ground Truth comparison w/ alignement
+evo_traj tum output/icp_reconstructed_traj.csv trajectories/imu_odom_traj1_timestamp_modified.csv --ref output/gt_reconstructed_traj.csv -pv --align_origin --full_check
+
+
+### ICP / Ground Truth comparison
+
+evo_traj tum output/icp_reconstructed_traj.csv --ref output/gt_reconstructed_traj.csv 
+
+evo_traj tum output/icp_reconstructed_traj.csv output/gt_reconstructed_traj.csv --plot_mode xy -p
+
+-v : verbose
+--full_check : verbose ++
+
+evo_config set plot_axis_marker_scale 0 : disable quiver
+evo_config set plot_axis_marker_scale 0.5 : enable quiver
--- scripts/trajectory_generation.py
+++ scripts/trajectory_generation.py
@@ -81,7 +81,7 @@ def generate_reconstructed_traj(gps_only_traj, rts_only_traj, common_traj):
     reconstructed_traj = pd.concat([common_traj, gps_only_traj, rts_only_traj], ignore_index=True)
     reconstructed_traj.sort_values(by=['Timestamp'], inplace=True)
 
-    return reconstructed_traj
+    return reconstructed_traj, T
 
 
 def pose_quat_to_tranform(dataframe):
@@ -114,31 +114,59 @@ def transform_to_pose_quat(dataframe):
     return dataframe
 
 
-def plot_reconstructed_traj(gps_only_traj, rts_only_traj, common_traj, reconstructed_traj, arrows=False):
+def plot_reconstructed_traj(df_gps, df_rts, gps_only_traj, rts_only_traj, common_traj, arrows=False):
 
-    fig ,axs = plt.subplots(2,2,figsize =(8,8))
-    colors = ['lightgrey', 'lightskyblue', 'lightsalmon', 'red']
-    labels = ['RTS only', 'GPS only', 'Common', 'Reconstructed']
-    trajs = [rts_only_traj, gps_only_traj, common_traj, reconstructed_traj]
-    for ax, traj, color, label in zip(axs.flat, trajs, colors, labels):
-        X = [T[0,3] for T in traj['T']]
-        Y = [T[1,3] for T in traj['T']]
-        ax.scatter(X, Y, s=2, c=color, label = label)
-        ax.title.set_text(label + ' trajectory')
-        ax.set_aspect('equal')
-        ax.legend()
+    X_origin = 245610
+    Y_origin = 5182375
+
+    plt.rc('font', family='serif', serif='Times')
+    plt.rc('text', usetex=True)
+    plt.rc('xtick', labelsize=20)
+    plt.rc('ytick', labelsize=20)
+    plt.rc('axes', labelsize=20)
+    plt.rcParams.update({'font.size': 20})
+    font_size = 20
+
+    
+    fig ,axs = plt.subplots(1,3,figsize =(15,5))
+    fig.subplots_adjust(left=.13, bottom=.13, right=.99, top=.99,wspace=3)
+    colors = ['lightskyblue', 'lightsalmon', 'lightgrey']
+    labels = ['GPS', 'RTS', 'Commune']
+    trajs = [gps_only_traj, rts_only_traj, common_traj]
+    titles = ['GNSS', 'RTS', 'Trajectoire reconstruite']
+
+    axs[0].scatter([T[0,3] for T in df_gps['T'] - X_origin], [T[1,3] for T in df_gps['T'] - Y_origin], s=2, c=colors[0], label = labels[0])
+    axs[1].scatter([T[0,3] for T in df_rts['T'] - X_origin], [T[1,3] for T in df_rts['T'] - Y_origin], s=2, c=colors[1], label = labels[1])
+    for i,traj in enumerate(trajs) :
+        X = [T[0,3] for T in traj['T'] - X_origin]
+        Y = [T[1,3] for T in traj['T'] - Y_origin]
+        axs[2].scatter(X, Y, s=2, c=colors[i], label = labels[i])
 
         if arrows:
             nx_u = [T[0,0] for T in traj['T']]
             nx_v = [T[1,0] for T in traj['T']]
             ny_u = [T[0,1] for T in traj['T']]
             ny_v = [T[1,1] for T in traj['T']]
-            ax.quiver(X, Y, nx_u, nx_v, color='r', scale=1, scale_units='xy', angles='xy', headwidth=1)
-            ax.quiver(X, Y, ny_u, ny_v, color='g', scale=1, scale_units='xy', angles='xy', headwidth=1)
+            axs[2].quiver(X, Y, nx_u, nx_v, color='r', scale=1, scale_units='xy', angles='xy', headwidth=1)
+            axs[2].quiver(X, Y, ny_u, ny_v, color='g', scale=1, scale_units='xy', angles='xy', headwidth=1)
 
-    plt.show()
+    last_ax = axs.flat[-1]
+    x_lim = last_ax.get_xlim()
+    y_lim = last_ax.get_ylim()
 
+    for ax, title in zip(axs.flat, titles):
+        ax.title.set_text(title)
+        ax.set_aspect('equal')
+        # ax.legend(loc='lower left')
+        ax.set_xlabel('X [m]')
+        ax.set_ylabel('Y [m]')
+        ax.set_xlim(x_lim)
+        ax.set_ylim(y_lim)
+        for axis in ['top','bottom', 'left', 'right']:
+            ax.spines[axis].set_linewidth(1.5)
 
+    fig.set_size_inches(13, 5.5)
+    
 def find_transform_between_origins(gt_traj, df_icp):
 
     merged_data = pd.merge_asof(
@@ -160,13 +188,16 @@ def find_transform_between_origins(gt_traj, df_icp):
 
 def plot_icp_vs_gt(icp_traj, gt_traj, arrows=False):
 
-    fig, ax = plt.subplots(1,1,figsize =(8,8))
-    colors = ['lightgrey', 'lightsalmon']
-    labels = ['ICP', 'Ground truth']
+    X_origin = 245610
+    Y_origin = 5182375
+
+    fig, ax = plt.subplots(1,1,figsize =(6,6))
+    colors = ['lightskyblue', 'mediumseagreen']
+    labels = ['ICP', 'Vérité terrain']
     trajs = [icp_traj, gt_traj]
     for traj, color, label in zip(trajs, colors, labels):
-        X = [T[0,3] for T in traj['T']]
-        Y = [T[1,3] for T in traj['T']]
+        X = [T[0,3] for T in traj['T'] - X_origin]
+        Y = [T[1,3] for T in traj['T'] - Y_origin]
         if arrows:
             nx_u = [T[0,0] for T in traj['T']]
             nx_v = [T[1,0] for T in traj['T']]
@@ -176,14 +207,15 @@ def plot_icp_vs_gt(icp_traj, gt_traj, arrows=False):
             ax.quiver(X, Y, ny_u, ny_v, color='g', scale=1, scale_units='xy', angles='xy', headwidth=1)
 
         ax.scatter(X, Y, s=2, c=color, label = label)
+        # ax.title.set_text('ICP trajectory comparaison with groundtruth')
+        ax.title.set_text('Comparaison de la trajectoire ICP avec la vérité terrain')
+        ax.set_xlabel('X [m]')
+        ax.set_ylabel('Y [m]')
         ax.set_aspect('equal')
-        ax.legend()
+        ax.legend(loc='lower left')
 
 
-    plt.show()
-
-
-def main(gps_file, rts_file, icp_file, plot, arrows, save):
+def main(gps_file, rts_file, icp_file, plot, save_plot, arrows, save, verbose):
 
     df_gps = pd.read_csv(BASE_PATH / f'{gps_file}', names=["Timestamp", "X", "Y", "Z", "qx", "qy", "qz", "qw"], delimiter= ' ')
     df_rts = pd.read_csv(BASE_PATH / f'{rts_file}', names=["Timestamp", "X", "Y", "Z", "qx", "qy", "qz", "qw"], delimiter= ' ')
@@ -192,41 +224,64 @@ def main(gps_file, rts_file, icp_file, plot, arrows, save):
     for df in [df_gps, df_rts, df_icp]:
         df = pose_quat_to_tranform(df)
     gps_only_traj, rts_only_traj, common_traj = split_rts_gps(df_gps, df_rts)
-    reconstructed_traj = generate_reconstructed_traj(gps_only_traj, rts_only_traj, common_traj)
-    if plot: 
-        plot_reconstructed_traj(gps_only_traj, rts_only_traj, common_traj, reconstructed_traj, arrows=arrows)
-    # reconstructed_traj.to_csv(BASE_PATH / 'trajectories' / 'gt_reconstructed_traj1.csv', index=False, header=False, sep = ' ')
+    reconstructed_traj, T = generate_reconstructed_traj(gps_only_traj, rts_only_traj, common_traj)
+    df_rts_transformed = df_rts.copy()
+    df_rts_transformed['T'] = df_rts_transformed['T'].apply(lambda x: T @ x)
+
+    if plot:
+        plot_reconstructed_traj(df_gps, df_rts_transformed, gps_only_traj, rts_only_traj, common_traj, arrows=arrows)
+        plt.tight_layout()
+        if save_plot :
+            plt.savefig(BASE_PATH / 'figures' / 'reconstructed_traj.pdf')
+        plt.show()
 
     T_icp_gt = find_transform_between_origins(reconstructed_traj, df_icp)
     df_icp['T'] = df_icp['T'].apply(lambda x: T_icp_gt @ x)
+
     if plot:
         plot_icp_vs_gt(df_icp, reconstructed_traj, arrows=arrows)
+        plt.tight_layout()
+        if save_plot :
+            plt.savefig(BASE_PATH / 'figures' / 'comparison_icp_gt.pdf')
+        plt.show()
 
     if save:
         df_icp = transform_to_pose_quat(df_icp)
         reconstructed_traj = transform_to_pose_quat(reconstructed_traj)
         df_icp.to_csv(BASE_PATH / 'output' / 'icp_reconstructed_traj.csv', index=False, header=False, sep = ' ')
         reconstructed_traj.to_csv(BASE_PATH / 'output' / 'gt_reconstructed_traj.csv', index=False, header=False, sep = ' ')
+    
+    if verbose :
+        print('Nombre de poses de la trajectoire uniquement GNSS :',len(gps_only_traj))
+        print('Nombre de poses de la trajectoire uniquement RTS :',len(rts_only_traj))
+        print('Nombre de poses de la trajectoire uniquement commune :',len(common_traj))
+        print('Nombre de poses de la trajectoire reconstruite :',len(reconstructed_traj))
+
+
 
 
 def init_argparse():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-gps', '--gps',
+    parser.add_argument('-gps',
                         type=str, required=True,
                         help='Name of gps file.')
-    parser.add_argument('-rts', '--rts',
+    parser.add_argument('-rts',
                         type=str, required=True,
                         help='Name of rts file.')
-    parser.add_argument('-icp', '--icp',
+    parser.add_argument('-icp',
                         type=str, required=True,
                         help='Name of icp file.')
     parser.add_argument('-p', '--plot', action='store_true',
                         help='Plot trajectories for debugging.')
+    parser.add_argument('--save_plot', action='store_true',
+                        help='save plot in results folder.')
     parser.add_argument('-a', '--arrows', action='store_true',
                         help='Plot arrows onto trajectories.')
     parser.add_argument('-s', '--save', action='store_true',
                         help='Save resulting trajectories in output folder.')
+    parser.add_argument('--verbose', action='store_true',
+                        help='Print informations about reconstructed trajectory.')
     return parser
 
 
@@ -234,4 +289,4 @@ if __name__ == '__main__':
 
     parser = init_argparse()
     args = parser.parse_args()
-    main(args.gps, args.rts, args.icp, args.plot, args.arrows, args.save)
+    main(args.gps, args.rts, args.icp, args.plot, args.save_plot, args.arrows, args.save, args.verbose)
